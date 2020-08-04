735c24867bab2d677782e6b0de3b46858ab102b3
silvan <silvan_kuttimalai@gmx.de>
2020-08-03 20:19:47 -0700

    Add observables to example

--- src/Example.cc
+++ src/Example.cc
@@ -37,25 +37,34 @@ int main(int argc, char** argv)
   SSM::EngineSpeed              engine_speed;
   SSM::ManifoldRelativePressure manifold_pressure;
   SSM::ExhaustGasTemperature    exhaust_gas_temp;
+  SSM::IntakeAirTemperature     intake_temp;
   SSM::AirFuelLeanCorrection    af_lean_corr;
   SSM::EngineLoad               en_load;
   SSM::KnockingCorrection       kn_corr;
   SSM::NeutralPositionSwitch    nt_sw;
+  SSM::KnockSignal1             knock_1;
+  SSM::KnockSignal2             knock_2;
+  
   SSM::Observables observables { &battery_voltage,
 				 &engine_speed,
 				 &manifold_pressure,
 				 &exhaust_gas_temp,
+				 &intake_temp,
 				 &af_lean_corr,
 				 &en_load,
 				 &kn_corr,
-				 &nt_sw };
+				 &nt_sw,
+				 &knock_1,
+				 &knock_2 };
 
   SSM::Values values = ECU.singleRead(observables);
   assert(observables.size() == values.size());
 
   std::cout << "Single read results:" << std::endl;
   for(int i(0); i<values.size(); i++)
-    std::cout << ECU.isAvailable(*observables[i]) << " | " << values[i] << " " << observables[i]->unit() << std::endl;
+    std::cout << ECU.isAvailable(*observables[i])
+	      << " | " << values[i] << " "
+	      << observables[i]->unit() << std::endl;
 
   ResultHandler handler(100);
   auto callback = std::bind(&ResultHandler::handle, 
